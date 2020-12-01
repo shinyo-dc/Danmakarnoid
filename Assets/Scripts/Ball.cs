44c666d62c3968a8283e62ce676a141ae4e69f80
shinyo-dc <duongchan159@gmail.com>
2020-12-01 22:25:15 +0700

    Added the hit animation and died animation, although not refined

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -9,8 +9,6 @@ public class Ball : MonoBehaviour
     [SerializeField] float startY = 0f;
     [SerializeField] float velX = -15f;
     [SerializeField] float velY = 30f;
-    float lastVelX = 0;
-    float lastVelY = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -36,16 +34,17 @@ public class Ball : MonoBehaviour
         if (other.gameObject.tag.Equals("Player"))
         {
             GameObject player = GameObject.FindGameObjectWithTag("Player");
+            float speed = player.GetComponent<Animator>().GetFloat("Speed");
             player.GetComponent<ReimuHealth>().TakeDamage();
             this.GetComponent<Rigidbody2D>().velocity = new Vector2(0f, 0f);
             this.GetComponent<Rigidbody2D>().AddForce(Vector2.up * 1000f);
             if (player.transform.localScale.x == -1)
             {
-                this.GetComponent<Rigidbody2D>().AddForce(Vector2.right * 1000f);
+                this.GetComponent<Rigidbody2D>().AddForce(Vector2.right * 20f * speed);
             }
             else if (player.transform.localScale.x == 1)
             {
-                this.GetComponent<Rigidbody2D>().AddForce(Vector2.left * 1000f);
+                this.GetComponent<Rigidbody2D>().AddForce(Vector2.left * 20f * speed);
             }
         }
     }
@@ -56,7 +55,14 @@ public class Ball : MonoBehaviour
             GameObject bullet = GameObject.FindGameObjectWithTag("Bullet");
             float distX = this.transform.position.x - bullet.transform.position.x;
             float velX = this.GetComponent<Rigidbody2D>().velocity.x;
-            this.GetComponent<Rigidbody2D>().velocity = new Vector2(velX, 0f);
+            if (velX > 0f)
+            {
+                this.GetComponent<Rigidbody2D>().velocity = new Vector2(velX, 0f);
+            }
+            else
+            {
+                this.GetComponent<Rigidbody2D>().velocity = new Vector2(distX*5f, 0f);
+            }
             this.GetComponent<Rigidbody2D>().AddForce(Vector2.up * 1000f);
         }
     }
--- Assets/Scripts/ReimuHealth.cs
+++ Assets/Scripts/ReimuHealth.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 
 public class ReimuHealth : MonoBehaviour
 {
-    public int health = 1;
-    public bool isInvulnerable = false; // when enter dash mode or slash mode
+    public int health = 3;
+    public bool isInvulnerable = false; // when enter dash mode or slash mode this is true
+    bool hit = false;
+    bool die = false;
+    [SerializeField] Animator animator;
 
     public void TakeDamage()
     {
+        Debug.Log(animator.name);
         if (isInvulnerable)
             return;
 
@@ -18,9 +22,30 @@ public class ReimuHealth : MonoBehaviour
         {
             Die();
         }
+        else
+        {
+            Hit();
+        }
+    }
+    
+    void Hit()
+    {
+        hit = true;
+        animator.SetBool("isHit", hit);
+    }
+    void HitDisable()
+    {
+        hit = false;
+        animator.SetBool("isHit", hit);
     }
 
     void Die()
+    {
+        die = true;
+        animator.SetBool("isDead", die);
+    }
+
+    void Destroy()
     {
         Destroy(gameObject);
     }
