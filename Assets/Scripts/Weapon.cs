5f294e9b0cf3d920b61ace5d3a286f26ede91250
shinyo-dc <duongchan159@gmail.com>
2020-12-01 08:31:17 +0700

    Limited character's movement and added shoot animation

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -6,17 +6,35 @@ public class Weapon : MonoBehaviour
 {
     public Transform firepoint;
     public GameObject bulletPrefab;
-    
+    Animator animator;
+    void Start()
+    {
+        animator = this.gameObject.GetComponent<Animator>();
+    }
+
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.C))
-        {
-            Shoot();
-        } 
+        ShootEnable();
     }
     void Shoot()
     {
         Instantiate(bulletPrefab, firepoint.position, firepoint.rotation);
+    }
+    void ShootEnable()
+    {
+        if (Input.GetKey(KeyCode.C))
+        {
+            animator.SetBool("isShooting", true);
+            if (animator.GetFloat("Speed") > 0.01f)
+            {
+                animator.SetBool("isShooting", false);
+            }
+        } 
+    }
+    void ShootDisable()
+    {
+        animator.SetBool("isShooting", false);
+        Shoot();
     }
 }
